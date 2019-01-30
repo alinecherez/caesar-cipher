401fab2471eb39c7b4442cdaf750c6589c11a8e6
Aline Cherez <alinecherez@gmail.com>
2019-01-30 14:33:26 -0200

    Versao 1, JS funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,58 @@
-window.cipher = {
-  // ... 
-};
+document.getElementById("botaoCodifica").addEventListener("click", cipherEncode);
+document.getElementById("botaoDecodifica").addEventListener("click", cipherDecode);
+
+//******
+
+function cipherEncode() {
+  let msgNormal = document.getElementById("mensagem").value;
+  let deslocamento = parseInt(document.getElementById("offset").value);
+  let resultado = cifra1(msgNormal,deslocamento);
+
+  document.getElementById("mensagemCodificada").innerHTML ="Sua mensagem é: " + resultado;
+}
+
+function cifra1 (msgNormal, deslocamento) {
+  let codLetra = "";
+  let msgCodificada = "";
+  for (let i = 0; i < msgNormal.length; i++) {
+    if (msgNormal.charCodeAt(i) >= 65 && msgNormal.charCodeAt(i) <=90){
+      codLetra = (((msgNormal.charCodeAt(i)-65)+deslocamento) %26) + 65;
+    }
+    else if (msgNormal.charCodeAt(i) >= 97 && msgNormal.charCodeAt(i) <=122){
+      codLetra = (((msgNormal.charCodeAt(i)-97)+deslocamento) %26) + 97;
+    }
+    else if (msgNormal.charCodeAt(i) === 32){
+      codLetra = 32;
+    }
+    msgCodificada += String.fromCharCode(codLetra);
+   }
+   return msgCodificada;
+}
+
+//******
+
+function cipherDecode() {
+  let msgNormal = document.getElementById("mensagem").value;
+  let deslocamento = parseInt(document.getElementById("offset").value);
+  let resultado = cifra2(msgNormal,deslocamento);
+
+  document.getElementById("mensagemCodificada").innerHTML ="Sua mensagem é: " + resultado;
+}
+
+function cifra2 (msgNormal, deslocamento) {
+  let codLetra = "";
+  let msgCodificada = "";
+  for (let i = 0; i < msgNormal.length; i++) {
+    if (msgNormal.charCodeAt(i) >= 65 && msgNormal.charCodeAt(i) <=90){
+      codLetra = (((((msgNormal.charCodeAt(i)-65)-deslocamento)%26)+26) %26) + 65;
+    }
+    else if (msgNormal.charCodeAt(i) >= 97 && msgNormal.charCodeAt(i) <=122){
+      codLetra = (((((msgNormal.charCodeAt(i)-97)-deslocamento)%26)+26) %26) + 97;
+    }
+    else if (msgNormal.charCodeAt(i) === 32){
+      codLetra = 32;
+    }
+    msgCodificada += String.fromCharCode(codLetra);
+   }
+   return msgCodificada;
+}
---
+++ src/tests.js
@@ -0,0 +1,23 @@
+    if (cifra1("Aline",2) === "Cnkpg") {
+        console.log("Funciona!");
+    } else {
+        console.log("Não funciona");
+    }
+   
+    if (cifra2("Cnkpg",2) === "Aline") {
+        console.log("Funciona!");
+    } else {
+        console.log("Não funciona")
+    }
+
+    if (cifra1("Zelador da casa",5) === "Ejqfitw if hfxf") {
+        console.log("Funciona!");
+    } else {
+        console.log("Não funciona");
+    }
+   
+    if (cifra2("Ejqfitw if hfxf",5) === "Zelador da casa") {
+        console.log("Funciona!");
+    } else {
+        console.log("Não funciona")
+    }
